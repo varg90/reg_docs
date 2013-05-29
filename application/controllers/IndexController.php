159e5537ba7cb52b0f2d39156555596c31005aec
varg90 <kuhn.alexey@gmail.com>
2013-05-29 23:48:16 +0800

    dancing with data from xls file

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -13,8 +13,17 @@ class IndexController extends Zend_Controller_Action
         $form = new Form_Upload;
         $this->view->form = $form;
         if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
-            $reader = new Spreadsheet_Excel_Reader($form->firstFile->getFileName());
-            echo $reader->dump(true, true);
+            $firstFileData = new Spreadsheet_Excel_Reader($form->firstFile->getFileName());
+//            $secondFileData = new Spreadsheet_Excel_Reader($form->secondFile->getFileName());
+            $sheets = $firstFileData->sheets[0];
+            $cells = $sheets['cells'];
+            $firstFileGrid = array();
+            foreach ($cells as $cell) {
+                if (gettype($cell[1]) == 'integer') {
+                    array_merge($firstFileGrid, $cell);
+            throw new Exception(Zend_Debug::dump(key($cell)));
+                }
+            }
         }
     }
 
